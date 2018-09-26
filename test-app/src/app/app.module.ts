21b619b10012e723d8df8ad7d057faaf61a412d6
ohuden <ohuden@gmail.com>
2018-09-26 16:19:29 +0300

    mock data + components

--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PopupComponent } from './components/popup/popup.component';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { FiltersComponent } from './components/filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PopupComponent,
+    DataTableComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule
